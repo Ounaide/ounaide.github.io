79f27ad96ec5d0aa42fb844fc22c071a755d8e19
Ounaide <69464156+Ounaide@users.noreply.github.com>
2023-04-17 13:30:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+  return "hello world"
+
+@app.route("/<path:p>")
+def catch_all(p):
+  return redirect(url_for("index"))
+  
+if __name__ == "__main__":
+  app.run(host="0.0.0.0")
